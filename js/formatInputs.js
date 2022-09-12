2172786fb91621b722d64cf8b525174ba8a6f27f
ar4ibais <ak.hutov@yandex.ru>
2022-09-12 22:35:33 +0400

    made a total amount calc

--- js/formatInputs.js
+++ js/formatInputs.js
@@ -1,6 +1,10 @@
+import {priceFormatter} from './formatters.js';
+
 const inputCost = document.querySelector('#input-cost'),
       inputDownPayment = document.querySelector('#input-downpayment'),
-      inputTerm = document.querySelector('#input-term');
+      inputTerm = document.querySelector('#input-term'),
+      form = document.querySelector('#form'),
+      totalCost = document.querySelector('#total-cost');
 
 const cleavePriceSettings = {
     numeral: true,
@@ -8,6 +12,17 @@ const cleavePriceSettings = {
     delimiter: ' '
 };
 
-const cleaveCost = new Cleave('#input-cost', cleavePriceSettings);
+const cleaveCost = new Cleave('#input-cost', cleavePriceSettings),
+      cleaveDownPayment = new Cleave('#input-downpayment', cleavePriceSettings);
 
-const cleaveDownPayment = new Cleave('#input-downpayment', cleavePriceSettings);
+calcMortgage();
+
+form.addEventListener('input', () => {
+    calcMortgage();
+});
+
+function calcMortgage() {
+    const totalAmount = +cleaveCost.getRawValue() - cleaveDownPayment.getRawValue();
+
+    totalCost.textContent = priceFormatter.format(totalAmount);
+}
--- js/formatters.js
+++ js/formatters.js
@@ -1,1 +1,2 @@
-export const percentFormatter = new Intl.NumberFormat('ru-RU', {style: 'percent', maximumFractionDigits: 3}); // 4,752%
+export const percentFormatter = new Intl.NumberFormat('ru-RU', {style: 'percent', maximumFractionDigits: 3}),
+             priceFormatter = new Intl.NumberFormat('ru-RU', {style: 'currency', currency: 'RUB', maximumFractionDigits: 2});
