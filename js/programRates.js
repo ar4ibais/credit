f62233265682ee9826d03f022341e9c1cf7fdad8
ar4ibais <ak.hutov@yandex.ru>
2022-09-12 21:43:29 +0400

    made credit rates

---
+++ js/programRates.js
@@ -0,0 +1,21 @@
+import { percentFormatter } from "./formatters.js";
+
+
+console.log(percentFormatter.format(0.567));
+// Ставки по ипотеке
+const programBase = 0.12,
+      programIt = 0.047,
+      programGov = 0.067,
+      programZero = 0.108;
+
+// Указываем ставку в радио-кнопках
+document.querySelector('#base-value').value = programBase;
+document.querySelector('#it-value').value = programIt;
+document.querySelector('#gov-value').value = programGov;
+document.querySelector('#zero-value').value = programZero;
+
+// Указываем ставку в label
+document.querySelector('#base-text').textContent = percentFormatter.format(programBase);
+document.querySelector('#it-text').textContent = percentFormatter.format(programIt);
+document.querySelector('#gov-text').textContent = percentFormatter.format(programGov);
+document.querySelector('#zero-text').textContent = percentFormatter.format(programZero);
