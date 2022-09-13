86d83ad0f7963078fd20478d47bc44d82f45e2e7
ar4ibais <ak.hutov@yandex.ru>
2022-09-13 23:40:42 +0400

    made a monthpayment

--- js/formatInputs.js
+++ js/formatInputs.js
@@ -1,10 +1,11 @@
-import {priceFormatter} from './formatters.js';
+import {priceFormatter, priceFormatterDecimals} from './formatters.js';
 
 const inputCost = document.querySelector('#input-cost'),
       inputDownPayment = document.querySelector('#input-downpayment'),
       inputTerm = document.querySelector('#input-term'),
       form = document.querySelector('#form'),
-      totalCost = document.querySelector('#total-cost');
+      totalCost = document.querySelector('#total-cost'),
+      totalMonthPayment = document.querySelector('#total-month-payment');
 
 const cleavePriceSettings = {
     numeral: true,
@@ -13,7 +14,8 @@ const cleavePriceSettings = {
 };
 
 const cleaveCost = new Cleave('#input-cost', cleavePriceSettings),
-      cleaveDownPayment = new Cleave('#input-downpayment', cleavePriceSettings);
+      cleaveDownPayment = new Cleave('#input-downpayment', cleavePriceSettings),
+      cleaveTerm = new Cleave(inputTerm, cleavePriceSettings);
 
 calcMortgage();
 
@@ -23,6 +25,15 @@ form.addEventListener('input', () => {
 
 function calcMortgage() {
     const totalAmount = +cleaveCost.getRawValue() - cleaveDownPayment.getRawValue();
-
     totalCost.textContent = priceFormatter.format(totalAmount);
+
+    const creditRate = +document.querySelector('input[name="program"]:checked').value,
+          monthRate = creditRate / 12;
+
+    const years = +cleaveTerm.getRawValue(),
+          months = years * 12;
+
+    const monthPayment = (totalAmount * monthRate) / (1 - (1 + monthRate) * (1 - months)) * 100;
+    
+    totalMonthPayment.textContent = priceFormatterDecimals.format(monthPayment);
 }
--- js/formatters.js
+++ js/formatters.js
@@ -1,2 +1,3 @@
 export const percentFormatter = new Intl.NumberFormat('ru-RU', {style: 'percent', maximumFractionDigits: 3}),
-             priceFormatter = new Intl.NumberFormat('ru-RU', {style: 'currency', currency: 'RUB', maximumFractionDigits: 2});
+             priceFormatter = new Intl.NumberFormat('ru-RU', {style: 'currency', currency: 'RUB', maximumFractionDigits: 2}),
+             priceFormatterDecimals = new Intl.NumberFormat('ru-RU', {style: 'currency', currency: 'RUB', maximumFractionDigits: 2});
