6faf13691b1f24d25578c56ec762d2993ec4a348
ar4ibais <ak.hutov@yandex.ru>
2022-09-14 23:15:20 +0400

    finished proj

--- js/formatInputs.js
+++ js/formatInputs.js
@@ -5,7 +5,8 @@ const inputCost = document.querySelector('#input-cost'),
       inputTerm = document.querySelector('#input-term'),
       form = document.querySelector('#form'),
       totalCost = document.querySelector('#total-cost'),
-      totalMonthPayment = document.querySelector('#total-month-payment');
+      totalMonthPayment = document.querySelector('#total-month-payment'),
+      maxPrice = 100000000;
 
 const cleavePriceSettings = {
     numeral: true,
@@ -24,7 +25,13 @@ form.addEventListener('input', () => {
 });
 
 function calcMortgage() {
-    const totalAmount = +cleaveCost.getRawValue() - cleaveDownPayment.getRawValue();
+    let cost = +cleaveCost.getRawValue();
+
+    if (cost > maxPrice) {
+        cost = maxPrice;
+    }
+
+    const totalAmount = cost - cleaveDownPayment.getRawValue();
     totalCost.textContent = priceFormatter.format(totalAmount);
 
     const creditRate = +document.querySelector('input[name="program"]:checked').value,
@@ -56,11 +63,86 @@ noUiSlider.create(sliderCost, {
     })
 });
 
-sliderCost.noUiSlider.on('update', function() {
+sliderCost.noUiSlider.on('slide', function() {
     const sliderValue = parseInt(sliderCost.noUiSlider.get(true));
 
     inputCost.value = sliderValue;
 
     cleaveCost.setRawValue(sliderValue);
     calcMortgage();
-})
+});
+
+
+let sliderDownpayment = document.querySelector('#slider-downpayment');
+
+noUiSlider.create(sliderDownpayment, {
+    start: 375000 ,
+    connect: 'lower',
+    step: 100000,
+    range: {
+        'min': 375000 * 0.15,
+        'max': maxPrice
+    },
+    format: wNumb({
+        decimals: 0,
+        thousand: ' ',
+        suffix: ''
+    })
+});
+
+sliderDownpayment.noUiSlider.on('slide', function() {
+    const sliderValue = parseInt(sliderDownpayment.noUiSlider.get(true));
+
+    inputDownPayment.value = sliderValue;
+
+    cleaveDownPayment.setRawValue(sliderValue);
+    calcMortgage();
+});
+
+
+let sliderTerm = document.querySelector('#slider-term');
+
+noUiSlider.create(sliderTerm, {
+    start: 1,
+    connect: 'lower',
+    step: 1,
+    range: {
+        'min': 1,
+        'max': 30
+    },
+    format: wNumb({
+        decimals: 0,
+        thousand: ' ',
+        suffix: ''
+    })
+});
+
+sliderTerm.noUiSlider.on('update', function() {
+    const sliderValue = parseInt(sliderTerm.noUiSlider.get(true));
+
+    inputTerm.value = sliderValue;
+
+    cleaveTerm.setRawValue(sliderValue);
+    calcMortgage();
+});
+
+inputCost.addEventListener('input', () => {
+    const value = +cleaveCost.getRawValue();
+
+    sliderCost.noUiSlider.set(value);
+
+    if (value > maxPrice) {
+        inputCost.closest('.param__details').classList.add('param__details--error');
+    } else {
+        inputCost.closest('.param__details').classList.remove('param__details--error');
+    }
+});
+
+inputCost.addEventListener('change', () => {
+    const value = +cleaveCost.getRawValue();
+
+    if (value > maxPrice) {
+        inputCost.closest('.param__details').classList.remove('param__details--error');
+        cleaveCost.setRawValue(maxPrice);
+    }
+});
